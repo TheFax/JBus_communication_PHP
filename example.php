a40a0faadd3ed9aff510eec3346348f6447d2bdf
TheFax <fabioslurp@yahoo.it>
2019-07-11 12:53:54 +0200

    Esempio non funzionate solo per appunti

--- example.php
+++ example.php
@@ -1,29 +1,77 @@
-/* Create a TCP/IP socket. */
-  /*http://php.net/manual/en/function.socket-create.php*/
-  $socket = socket_create(AF_INET, SOCK_STREAM, 0) or die("Could not create socket<br>"); 
-  socket_set_option($socket, SOL_SOCKET, SO_RCVTIMEO, array('sec' => 1, 'usec' => 500));
-  socket_set_option($socket, SOL_SOCKET, SO_SNDTIMEO, array('sec' => 1, 'usec' => 500));
+<?php
 
-//echo "Attempting to connect...";
-  $result = @socket_connect($socket, $host, $port) or die("Could not connect socket<br>"); 
-  
-  //echo "Sending request... ";
-  //Richiedo 48 byte (=0x30) partendo dall'indirizzo 0x1450;
-  //Il frame diventerà questo: chr(0x01) . chr(0x03) .chr(0x14) .chr(0x50) .chr(0x00) .chr(0x30) .chr(0x40) .chr(0x3F);
-  $in = frameGeneratorRead(0x1450, 0x30); 
-  socket_write($socket, $in, strlen($in)) or die("Could not write into the socket<br>");
-  
-  echo bin2hex($in);
-  
-//echo "Reading response:<br>";
-  //Riceverò una risposta Jbus che salverò dentro la variabile $out
-  $out = '';
-  $out = socket_read($socket, 2048) or die("Could not read from socket<br>");
-  
-  echo bin2hex($out);
-  
+require_once("jbus.php");
+require_once("jsocket.php");
+
+$esito = "OK - comando eseguito";
+
+$simulator_socket=jbusSocketOpen($_GET["ip"], 1025);
+
+$unlock_packet =  frameGeneratorDataWrite(0x2692, hex2bin("386E8A0B"));
+$response = jbusSocketCommunication($simulator_socket, $unlock_packet);
+if (jbusResponseCheck($unlock_packet, $response) == 0) $esito = "STOP! Non eseguito";
+
+
+$unlock_packet =  frameGeneratorDataWrite(0x1F12, hex2bin("386E8A0B"));
+$response = jbusSocketCommunication($simulator_socket, $unlock_packet);
+if (jbusResponseCheck($unlock_packet, $response) == 0) $esito = "STOP! Non eseguito";
+
+
+if ($_GET["command"] == "poweroff") {
+  //Spengo simulatore
+  $packet = frameGeneratorCommandWrite(0x25D9, 0x0080);
+}
+
+if ($_GET["command"] == "poweron") {
+  //Accendo simulatore
+  $packet = frameGeneratorCommandWrite(0x25D8, 0x0002);
+}
+
+if ($_GET["command"] == "set") {
+//Imposto simulatore con tensione e corrente date via $_GET
+//Costruisco il pacchetto:
+  $voltage = $_GET["voltage"];
+  $current = $_GET["current"];
+
+  if ( !is_numeric($voltage) ) die("Parametro tensione non numerico.");
+  if ( !is_numeric($current) ) die("Parametro corrente non numerico.");
+  if ( strlen($voltage)>3 ) die("Parametro tensione troppo lungo.");
+  if ( strlen($current)>3 ) die("Parametro corrente troppo lungo.");
+  if ( $voltage < 100 ) die("Parametro tensione troppo piccolo.");
+  if ( $voltage > 700 ) die("Parametro tensione troppo alto.");
+  if ( $current < 8  ) die("Parametro corrente troppo basso.");
+  if ( $current > 50  ) die("Parametro corrente troppo alto.");
+
+  $voltage = $voltage * 10;
+  $current = $current * 10;
+
+  $data_frame = "";
+  $data_frame .= hex2bin("0000"); //CmdAte
+  $data_frame .= hex2bin("0001"); //PV_Logic
+  $data_frame .= hex2bin("09C4"); //V_DC
+  $data_frame .= chr($current >> 8 & 0xFF) . chr($current & 0xFF); //I_ShortCircuit
+  $data_frame .= chr($voltage >> 8 & 0xFF) . chr($voltage & 0xFF); //Voc_Max_x_pannel
+  $data_frame .= hex2bin("0001"); //Num_of_pannels_in_string
+  $data_frame .= hex2bin("0001"); //uiDwell_time
+  $data_frame .= hex2bin("03e8"); //Irradiance
+	
+  $packet = frameGeneratorDataWrite(0x2770, $data_frame);
+  $esito = "Settato per: \n " . $_GET["voltage"] . " Volt\n " . $_GET["current"] . " Ampere";
+}
+
+$response = jbusSocketCommunication($simulator_socket, $packet);
+if (jbusResponseCheck($packet, $response) == 0) $esito = "STOP! Non eseguito";
+
+jbusSocketClose($simulator_socket);
+
+echo $esito;
+
+
+
+
+echo bin2hex($out);
 //Ora converto la risposta in un array
-  $splittata_rectifier = str_split($out);
+$splittata_rectifier = str_split($out);
   
 //In questo punto del programma ho a disposizione l'array $splittata_rectifier, che è a tutti gli effetti un array
 //corrispondente byte-per-byte alla risposta Jbus.
